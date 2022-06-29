09d29c7f2694aaa5ce8243bea4e75a41987ae58f
David RG <preparacion.dave@gmail.com>
2022-06-29 22:22:21 +0000

    POO Interfaces

---
+++ abstract/Base.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Base
+{
+    protected $name;
+    
+    private function getClassName()
+    {
+        return get_called_class();
+    }
+    
+    public function login()
+    {
+        return "<p>Mi nombre es $this->name desde la clase {$this->getClassName()} <br></p>";
+    }
+}
--- abstract/Index.php
+++ abstract/Index.php
@@ -1,41 +1,8 @@
 <?php
+require_once "Guest.php";
+require_once "Admin.php";
+require_once "User.php";
 
-abstract class Base
-{
-    protected $name;
-    
-    private function getClassName()
-    {
-        return get_called_class();
-    }
-    
-    public function login()
-    {
-        return "Mi nombre es $this->name desde la clase {$this->getClassName()}";
-    }
-}
-
-class Admin extends Base
-{
-    public function __construct($name)
-    {
-        $this->name = $name;
-    }
-}
-
-class User extends Base
-{
-    public function __construct($name)
-    {
-        $this->name = $name;
-    }
-}
-
-class Guest extends Base{
- 
-        protected $name = 'Invitado';
-
-}
 
 $guest = new Guest();
 echo $guest->login();
